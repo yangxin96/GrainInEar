884ca410d4b2845e0831bd8f7f24c53275709c21
yangxin96 <15071714190@163.com>
2022-06-04 10:19:30 +0800

    上传

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ module.exports = {
     // 网站的一些基本配置
     // base:配置部署站点的基础路径，后续再介绍
     title: '小巷', // 网站的标题
+    base: 'GrainInEar',
     description: '文档网站', // 网站的描述，它将会以 <meta> 标签渲染到当前页面的 HTML 中。
     sidebar: 'auto',
     head:[['link',{rel:'icon',href:'/log.png'}]], //浏览器标签页图标
